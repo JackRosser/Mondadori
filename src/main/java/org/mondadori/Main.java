caddc1c26f9cbf2ec50695bd672fe6d827b35986
Giacomo Rossettini <rossegiac@gmail.com>
2024-12-06 10:38:17 +0100

    generated DAO

--- src/main/java/org/mondadori/Main.java
+++ src/main/java/org/mondadori/Main.java
@@ -1,39 +1,29 @@
 package org.mondadori;
 
-import org.mondadori.entities.*;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import org.mondadori.DAO.LibroDAO;
+import org.mondadori.entities.Libro;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("unit");
-        EntityManager em = emf.createEntityManager();
+        LibroDAO libroDAO = new LibroDAO();
 
-        try {
-            em.getTransaction().begin();
+        Libro libro = new Libro();
+        libro.setIsbn("978-3-16-148410-0");
+        libro.setTitolo("Il Signore degli Anelli");
+        libro.setAnnoPubblicazione(1954);
+        libro.setNumeroPagine(1178);
+        libro.setAutore("J.R.R. Tolkien");
+        libro.setGenere("Fantasy");
 
-            Libro libro = new Libro();
-            libro.setIsbn("978-3-16-148410-0");
-            libro.setTitolo("Il Signore degli Anelli");
-            libro.setAnnoPubblicazione(1954);
-            libro.setNumeroPagine(1178);
-            libro.setAutore("J.R.R. Tolkien");
-            libro.setGenere("Fantasy");
+        libroDAO.save(libro);
 
-            em.persist(libro);
-
-            em.getTransaction().commit();
-
-            Libro savedBook = em.find(Libro.class, "978-3-16-148410-0");
+        Libro savedBook = libroDAO.findById("978-3-16-148410-0");
+        if (savedBook != null) {
             System.out.println("Libro salvato: " + savedBook.getTitolo());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            em.getTransaction().rollback();
-        } finally {
-            em.close();
-            emf.close();
+        } else {
+            System.out.println("Libro non trovato.");
         }
+
+        libroDAO.close();
     }
 }
